f979672f840283a2c8500cafbf9f4da956b59a07
Sarah Riethmueller <>
2025-08-31 11:46:01 +0200

    improve alpha calc in vertex shader

--- src/app/computer-model/model-scene.ts
+++ src/app/computer-model/model-scene.ts
@@ -218,20 +218,14 @@ export class ModelScene {
     return `
     varying float alpha;
     uniform float alphaBase;
-    void main()
-    {
-        //calculate the vertex position as expected in a perpective renderer
-        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        //now onto the alpha calculation
-        //get the vertex vertex normal
-        vec3 nmal = normal;
-        // find vector from the vertex to camera and then normalise it
-        vec3 viewVector =  normalize((cameraPosition - normal));
-        // now find the dot poduct
-        // faceing the camera is 1 and perpendicular is 0 so for this effect you want to minus it from 1
-        // note that alpha is the varying float passed between the vertex and fragment shader
-        alpha =  alphaBase - dot(nmal, viewVector);
-     }
+    void main() {
+      vec4 mvPos = modelViewMatrix * vec4(position, 1.0);
+      gl_Position = projectionMatrix * mvPos;
+
+      vec3 nmal = normalize(normalMatrix * normal);
+      vec3 viewVector = normalize(-mvPos.xyz);
+      alpha = alphaBase - dot(nmal, viewVector);
+    }
     `;
   }
 
